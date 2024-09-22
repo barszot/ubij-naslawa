a6fb6080c5f26c78a114f99e396c7aeda9c387a6
bjazwiec <bjazwiec.stud@pw.edu.pl>
2024-09-22 02:27:34 +0200

    koniec, teraz testy i deploy

--- README.md
+++ README.md
@@ -4,12 +4,5 @@ npx http-server -c-1
 
 # TODO
 
-1. młotek zabija nasławy (zerknij na todo w klasie mole)
-2. punkty za trafienie, kara za chybienie
-3. wyswietlanie punktów i rekordu
-4. możliwość powtórzenia gry
-5. zapisywanie rekordu
-
-6. ?? ranking??
-
-7. deploy
+1. testy
+2. deploy
--- game.js
+++ game.js
@@ -1,96 +1,91 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 let runningGame = false;
-let loadedImages = 0;  // Zmienna do liczenia załadowanych obrazów
-
-// Nasłuchuj kliknięcia na canvasie
-canvas.addEventListener('click', function() {
-    if (loadedImages >= 3) {
-        startGame();  // Wywołaj funkcję startGame() po kliknięciu, tylko jeśli wszystkie obrazy są załadowane
-    }
-});
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 // Definicja obrazów
 const startBackgroundImage = new Image();
-startBackgroundImage.src = 'graphics/start_background.png';  // Ścieżka do obrazka
-
+startBackgroundImage.src = 'graphics/start_background.png';
 const gameBackgroundImage = new Image();
-gameBackgroundImage.src = 'graphics/game_background.png';  // Ścieżka do obrazka
-
+gameBackgroundImage.src = 'graphics/game_background.png';
 const spriteSheet = new Image();
-spriteSheet.src = 'graphics/sprite_sheet.png';  // Ścieżka do sprite sheet'a
+spriteSheet.src = 'graphics/sprite_sheet.png';
+const defaultEndBackground = new Image();
+defaultEndBackground.src = 'graphics/default_end_background.png';
+const newRecordEndBackground = new Image();
+newRecordEndBackground.src = 'graphics/new_record_end_background.png';
 
-// Funkcje wczytania obrazów
-startBackgroundImage.onload = function() {
-    ++loadedImages;
-    ctx.drawImage(startBackgroundImage, 0, 0, canvas.width, canvas.height);  // Rysuje obraz jako tło na początku
-};
+let loadedImages = 0;
+// Funkcja do sprawdzenia, czy wszystkie obrazy są załadowane
+function imageLoaded() {
+    loadedImages++;
+    if (loadedImages === 5) {
+        // Wszystkie obrazy załadowane, rozpocznij grę
+        ctx.drawImage(startBackgroundImage, 0, 0, canvas.width, canvas.height);  // Rysuje obraz jako tło na początku
+        canvas.addEventListener('click', manageClicking);
+    }
+}
 
-gameBackgroundImage.onload = function() {
-    ++loadedImages;
-};
+// Dodaj nasłuchiwacze na załadowanie obrazów
+startBackgroundImage.onload = imageLoaded;
+gameBackgroundImage.onload = imageLoaded;
+spriteSheet.onload = imageLoaded;
+defaultEndBackground.onload = imageLoaded;
+newRecordEndBackground.onload = imageLoaded;
 
-spriteSheet.onload = function() {
-    ++loadedImages;
-};
 
-
+let mouseX = 900/2;
+let mouseY = 700/2;
 
 
 const RECT_WIDTH = 128;
 const RECT_HEIGHT = 162;
 
+
+function rectanglesCollide(rect1, rect2) {
+    return (
+        rect1.x <= rect2.x + rect2.width &&    // Sprawdź, czy lewy brzeg rect1 jest na lewo od prawego brzegu rect2
+        rect1.x + rect1.width >= rect2.x &&    // Sprawdź, czy prawy brzeg rect1 jest na prawo od lewego brzegu rect2
+        rect1.y <= rect2.y + rect2.height &&   // Sprawdź, czy górny brzeg rect1 jest na górze od dolnego brzegu rect2
+        rect1.y + rect1.height >= rect2.y       // Sprawdź, czy dolny brzeg rect1 jest na dole od górnego brzegu rect2
+    );
+}
+
 class Mole {
-    constructor(index, x, y)
-    {
+    constructor(index, x, y) {
         this.index = index;
         this.x = x;
         this.y = y;
         this.moleWidth = RECT_WIDTH;
         this.moleHeight = RECT_HEIGHT;
+        this.rect = { x: this.x, y: this.y, width: RECT_WIDTH, height: RECT_HEIGHT };
     }
-    draw(state)
-    {
+    draw(state) {
         if (state <= 0) {
             ctx.drawImage(spriteSheet, 386-28-this.moleWidth, 325-14-this.moleHeight, this.moleWidth, this.moleHeight, this.x, this.y, this.moleWidth, this.moleHeight);
         }
-        else if(state > this.lifespan*0.95 || state < this.lifespan*0.05)
-        {
+        else if(state > this.lifespan*0.95 || state < this.lifespan*0.05) {
             ctx.drawImage(spriteSheet, 386, 325-14-this.moleHeight, this.moleWidth, this.moleHeight, this.x, this.y, this.moleWidth, this.moleHeight);
         }
-        else if(state > self.lifespan*0.9 || state < self.lifespan*0.1)
-        {
+        else if(state > this.lifespan*0.9 || state < this.lifespan*0.1) {
             ctx.drawImage(spriteSheet, 386+28+this.moleWidth, 325-14-this.moleHeight, this.moleWidth, this.moleHeight, this.x, this.y, this.moleWidth, this.moleHeight);
         }
-        else
-        {
+        else {
             ctx.drawImage(spriteSheet, 386-28-this.moleWidth, 325, this.moleWidth, this.moleHeight, this.x, this.y, this.moleWidth, this.moleHeight);
         }
     }
-    setLifespan(newLifespan)
-    {
+    setLifespan(newLifespan) {
         this.lifespan = newLifespan
     }
-    //TODO
-    /*
-    self.rect = pygame.Rect(x, y, RECT_WIDTH, RECT_HEIGHT)
 
-    def is_clicked(self, event):
+    isClicked() {
+        let hammerRect = { x: mouseX - RECT_WIDTH/2, y: mouseY - RECT_HEIGHT/2, width: Math.floor(0.35*RECT_WIDTH), height: Math.floor(0.35*RECT_HEIGHT) };
+        return rectanglesCollide(this.rect, hammerRect);
+    }
 
-        hammer_rect = pygame.Rect(
-            event.pos[0] - RECT_WIDTH/2,
-            event.pos[1] - RECT_HEIGHT/2,
-            int(0.35*RECT_WIDTH),
-            int(0.35*RECT_HEIGHT)
-        )
-
-        if self.rect.colliderect(hammer_rect):
-            return True
-        else:
-            return False
-    */
 }
 
+let lifespan = 0;
 
 class MolesBoard
 {
@@ -147,7 +142,7 @@ class MolesBoard
         return newMolesIndexes.map(index => shuffledHoleIndexes[index]);
     }
 
-    updateBoard(lifespan, deltaTime) {
+    updateBoard(deltaTime) {
         const holeIndexes = [];
         for (let i = 0; i < 9; i++) {
             if (this.states[i] === 0) {
@@ -156,7 +151,7 @@ class MolesBoard
                 this.states[i] = Math.max(0, this.states[i]-deltaTime);
             }
             else {
-                this.states[i] = 0;
+                this.states[i] = Math.min(0, this.states[i]+deltaTime)
             }
         }
     
@@ -164,12 +159,34 @@ class MolesBoard
     
         for (const index of newMolesIndexes) {
             this.states[index] = lifespan;
+            this.moles[index].setLifespan(lifespan);
         }
     }
 }
 
-let molesBoard = new MolesBoard(3, 0.005);
+let score = 0;
+let molesBoard = new MolesBoard(4, 0.003);
 
+canvas.style.cursor = 'default';
+function manageClicking() {
+    if (runningGame === false) {
+        startGame();
+    }
+    else {
+        score -= 5;
+        for (let index = 0; index < molesBoard.moles.length; index++) {
+            const mole = molesBoard.moles[index];
+            if (mole.isClicked())  {
+                    if(molesBoard.states[index] > 0){
+                    molesBoard.states[index] = -lifespan / 2.0;  // Ustaw stan kreta na -lifespan/2.0
+                    score += 10;                                 // Dodaj punkty
+                    break;                                       // Przerwij pętlę po obsłużeniu kliknięcia
+                }
+            }
+
+        }
+    }
+}
 
 class Hammer
 {
@@ -185,7 +202,7 @@ class Hammer
     {
         return this.rotated;
     }
-    draw(mouseX, mouseY){
+    draw(){
         if(!this.rotated)
         {
             ctx.drawImage(spriteSheet, 386, 325, RECT_WIDTH, RECT_HEIGHT, mouseX-RECT_WIDTH/2, mouseY-RECT_HEIGHT/2, RECT_WIDTH, RECT_HEIGHT);
@@ -197,74 +214,112 @@ class Hammer
     }
 }
 
+
+let highscore = localStorage.getItem('highscore') ? parseInt(localStorage.getItem('highscore')) : 0;
+
+
+
+function drawScores(remainingTime) {
+
+    ctx.font = '42px "ComicSansCustom"';
+    ctx.fillStyle = 'black';
+    ctx.fillText("Wynik:  " + score, 15, 50);
+    ctx.fillText("Rekord: " + highscore, 15, 100);
+    ctx.font = '84px "ComicSansCustom"';
+    ctx.fillText(remainingTime, 785, 92);
+}
+
+let elapsedTime = 0; // Czas, który upłynął w sekundach
+let endGameClickHandler;
 let hammer = new Hammer();
 
-let mouseX = 900/2;
-let mouseY = 700/2;
-
-async function startGame() {
-
-    canvas.addEventListener('mousemove', (event) => {
-        mouseX = event.offsetX;
-        mouseY = event.offsetY;
-    });
-    canvas.addEventListener('mousedown', (event) => {
-        event.preventDefault(); // Zapobiega zaznaczaniu tekstu na stronie
-
-        hammer.setRotate(true);
-    });
     
-    canvas.addEventListener('mouseup', (event) => {
-        hammer.setRotate(false);
 
-    });
+let startGame = function () {
 
-    let score = 0;
-    secondsOfGame = 0;
-    molesBoard.draw();
-    canvas.style.cursor = 'none'; // Ukrywa kursor
-    runningGame = true;
-    requestAnimationFrame(gameLoop);
+    if (runningGame === false){
+
+        // Usuń poprzedni nasłuchiwacz, jeśli istnieje
+        if (endGameClickHandler) {
+            canvas.removeEventListener('click', endGameClickHandler);
+        }
+
+        canvas.addEventListener('mousemove', (event) => {
+            mouseX = event.offsetX;
+            mouseY = event.offsetY;
+        });
+        canvas.addEventListener('mousedown', (event) => {
+            event.preventDefault(); // Zapobiega zaznaczaniu tekstu na stronie
+            hammer.setRotate(true);
+        });
+        
+        canvas.addEventListener('mouseup', (event) => {
+            hammer.setRotate(false);
+        });
+
+        elapsedTime = 0;
+        score = 0;
+        canvas.style.cursor = 'none'; // Ukrywa kursor
+        runningGame = true;
+        requestAnimationFrame(gameLoop);
+    }
 }
+
 
 let lastTimestamp = 0;
 const targetFps = 60;
 const frameDuration = 1000 / targetFps; // czas na klatkę w ms
 const gameDuration = 60;
-let elapsedTime = 0; // Czas, który upłynął w sekundach
-
 
 // Główna pętla gry
 function gameLoop(timestamp) {
     // W rysowaniu młotka użyj aktualnej pozycji kursora
 
-    if (!runningGame) return;
+    if (!runningGame) endGame();
+    else {
+        const delta = timestamp - lastTimestamp;
+        if (delta >= frameDuration) {
 
-    const delta = timestamp - lastTimestamp;
+            lastTimestamp = timestamp - (delta % frameDuration); // Ustaw czas ostatniej klatki
+            elapsedTime += delta / 1000; // Zaktualizuj czas gry (w sekundach)
 
-    if (delta >= frameDuration) {
-
-        lastTimestamp = timestamp - (delta % frameDuration); // Ustaw czas ostatniej klatki
-        elapsedTime += delta / 1000; // Zaktualizuj czas gry (w sekundach)
-
-        if (elapsedTime >= gameDuration) {
-            runningGame = false; // Zatrzymaj grę po 60 sekundach
-            alert("Gra się skończyła!"); // Możesz zmienić to na inny sposób powiadamiania
-        } else {
-            let lifespan = Math.floor((100 - elapsedTime)/4);
-            molesBoard.updateBoard(lifespan, delta / 1000);
-            molesBoard.draw();
-            hammer.draw(mouseX, mouseY);
-
+            if (elapsedTime >= gameDuration) {
+                runningGame = false; // Zatrzymaj grę po 60 sekundach
+            } else {
+                lifespan = (100.0 - elapsedTime)/20.0;
+                molesBoard.updateBoard(delta / 1000);
+                molesBoard.draw();
+                hammer.draw();
+                drawScores(gameDuration - Math.floor(elapsedTime));
+            }
         }
-    }
 
-    requestAnimationFrame(gameLoop); // Wywołaj pętlę znowu
+        requestAnimationFrame(gameLoop); // Wywołaj pętlę znowu
+    }
+}   
+
+function drawEndScores()
+{
+        ctx.font = '64px "ComicSansCustom"';
+        ctx.fillStyle = 'yellow';
+        ctx.fillText(score, 575, 360);
+        ctx.fillText(highscore, 725, 465);
 }
 
-// Rozpocznij pętlę gry
-requestAnimationFrame(gameLoop);
+function endGame()
+{
+    if (score <= highscore){
+        ctx.drawImage(defaultEndBackground, 0, 0, canvas.width, canvas.height);
+    }
+    else {
+        highscore = score;
+        localStorage.setItem('highscore', highscore);
+        ctx.drawImage(newRecordEndBackground, 0, 0, canvas.width, canvas.height);
 
+    }
+    drawEndScores();
+    canvas.style.cursor = 'default';
+}
 
 
 const Slonik = `
